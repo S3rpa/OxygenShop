7f5a138495db685df842fc9adaa4cc3b139be6be
S3rpa <sergiobarbera1@gmail.com>
2024-06-25 13:35:13 +0200

    class slider

--- js/slider.js
+++ js/slider.js
@@ -1,78 +1,88 @@
-const sliderImages = document.querySelectorAll('.slider-container__slider img');
-const next = document.getElementById('next');
-const prev = document.getElementById('prev');
-const dots = document.querySelectorAll('.dots__container__dot');
-let counter = 0;
-let autoplayInterval;
+class Slider{
+    constructor (){
+    this.sliderImages = document.querySelectorAll('.slider-container__slider img');
+    this.next = document.getElementById('next');
+    this.prev = document.getElementById('prev');
+    this.dots = document.querySelectorAll('.dots__container__dot');
+    this.counter = 0;
+    this.autoplayInterval = null;
 
-// Inicialización del autoplay
-const startAutoplay = () => {
-    autoplayInterval = setInterval(slideNext, 5000); 
-};
+    // Damos un valor this a la función
 
-const stopAutoplay = () => {
-    clearInterval(autoplayInterval);
-};
+    this.slideNext = this.slideNext.bind(this);
+    this.slidePrev = this.slidePrev.bind(this);
+    this.startAutoplay = this.startAutoplay.bind(this);
+    this.stopAutoplay = this.stopAutoplay.bind(this);
 
-// Función para deslizar a la siguiente imagen
-const slideNext = () =>{
+    this.clickDots();
 
-    sliderImages[counter].classList.remove('active');
-    dots[counter].classList.remove('active');
-    sliderImages[counter].style.animation = 'next1 0.5s ease-in forwards';
+    this.startAutoplay();
+    }
 
-    // Actualizar el contador
-    counter = (counter + 1) % sliderImages.length;
+    startAutoplay() {
+        this.autoplayInterval = setInterval(this.slideNext, 5000); 
+    };
 
-    // Añadir clase activa y animación a la siguiente imagen
+    stopAutoplay(){
+        clearInterval(this.autoplayInterval);
+    };
 
-    sliderImages[counter].classList.add('active');
-    dots[counter].classList.add('active');
-    sliderImages[counter].style.animation = 'next2 0.5s ease-in forwards';
-};
+    slideNext() {
+        this.sliderImages[this.counter].classList.remove('active');
+        this.dots[this.counter].classList.remove('active');
+        this.sliderImages[this.counter].style.animation = 'next1 0.5s ease-in forwards';
+    
+        // Actualizar el contador
+        this.counter = (this.counter + 1) % this.sliderImages.length;
+    
+        // Añadir clase activa y animación a la siguiente imagen
+    
+        this.sliderImages[this.counter].classList.add('active');
+        this.dots[this.counter].classList.add('active');
+        this.sliderImages[this.counter].style.animation = 'next2 0.5s ease-in forwards';
+    }
+    slidePrev(){
 
+        this.sliderImages[this.counter].classList.remove('active');
+        this.dots[this.counter].classList.remove('active');
+        this.sliderImages[this.counter].style.animation = 'prev1 0.5s ease-in forwards';
+    
+        // Actualizar el contador
+        this.counter = (this.counter - 1 + sliderImages.length) % sliderImages.length;
+    
+        this.sliderImages[this.counter].classList.add('active');
+        this.dots[this.counter].classList.add('active');
+        this.sliderImages[this.counter].style.animation = 'prev2 0.5s ease-in forwards';
+    }
+    clickDots() {
+        // Clic en los puntos de navegación
+        this.dots.forEach((dot, index) => {
+            dot.addEventListener('click', () => {
+                this.stopAutoplay();
+                this.sliderImages[this.counter].classList.remove('active');
+                this.dots[this.counter].classList.remove('active');
 
-const slidePrev = () => {
+                this.counter = index;
 
-    sliderImages[counter].classList.remove('active');
-    dots[counter].classList.remove('active');
-    sliderImages[counter].style.animation = 'prev1 0.5s ease-in forwards';
+                this.sliderImages[this.counter].classList.add('active');
+                this.dots[this.counter].classList.add('active');
+                this.startAutoplay();
+            });
+        });
 
-    // Actualizar el contador
-    counter = (counter - 1 + sliderImages.length) % sliderImages.length;
-
-    sliderImages[counter].classList.add('active');
-    dots[counter].classList.add('active');
-    sliderImages[counter].style.animation = 'prev2 0.5s ease-in forwards';
-};
-
-// Añadir funcionalidad de clic en los puntos de navegación
-dots.forEach((dot, index) =>{
-
-    dot.addEventListener('click', () => {
-        stopAutoplay();
-        sliderImages[counter].classList.remove('active');
-        dots[counter].classList.remove('active');
-
-        counter = index;
-
-        sliderImages[counter].classList.add('active');
-        dots[counter].classList.add('active');
-        startAutoplay();
+    this.next.addEventListener('click', () => {
+        this.stopAutoplay();
+        this.slideNext();
+        this.startAutoplay();
     });
-});
+    
+    this.prev.addEventListener('click', () => {
+        this.stopAutoplay();
+        this.slidePrev();
+        this.startAutoplay();
+    });
+    }
+}
+// Crear instancia del slider
+const slider = new Slider();
 
-next.addEventListener('click', () => {
-    stopAutoplay();
-    slideNext();
-    startAutoplay();
-});
-
-prev.addEventListener('click', () => {
-    stopAutoplay();
-    slidePrev();
-    startAutoplay();
-});
-
-// Iniciar el autoplay al cargar la página
-startAutoplay();
